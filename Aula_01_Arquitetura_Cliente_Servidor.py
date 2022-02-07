63b262679fd20224044833f9dffb9bf69a43dbb9
Sak <joaovictoralvescosta051@gmail.com>
2022-02-06 21:33:28 -0300

    Conteudo 06/02/22

---
+++ Aula_01_Arquitetura_Cliente_Servidor.py
@@ -0,0 +1,20 @@
+"""
+Arquitetura Cliente Servidor
+
+Estrutura de computação distribui tarefas e cargas de trabalhos entre fornecedfores serviço, chamados de servidores e os requerentes dos serviços.
+
+---------------------------------------------------------------------------------------
+
+É importante falar sobre, pois estamos trabalhando com sistemas web. 
+As pessoas que vão utilizar o sistema, vão utilizar clients para conectar ao sistema desejado que está alocado em um servidor
+
+
+Acesso cliente -> internet/sistema---|
+                                     |--> Servidor
+
+
+Acesso cliente -> E sse acesso é feito pelo navegador. 
+internet(sistema)
+
+
+"""
---
+++ Aula_02_Protocolo_Https_e_Seus_Verbos.py
@@ -0,0 +1,24 @@
+"""
+Protocolo Http e seus verbos.
+
+Um dos protoolos mais importantes e utilizdados no mundo, o Protocolo de transferência de HiperTexto é o que usamos todos os dias,
+mesmo sem saber, para fazer acesso a todos os websites e redes sociais que conhecemos.
+
+
+Os serviços que implementam estre protocolo, chamados de serviços http, ou servidores http, se comunicam nas portas 80 para http,
+443 para https.
+
+Você encontra toda a refêrencia para este protocola atualmente na versão 2,
+na rfc 7540[1] que foi definida pela ietf - internet engineering task force
+
+https: criptografia
+http: padrão
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Http:
+
+HTTP é um protocolo (protocol) que permite a obtenção de recursos, como documentos HTML. 
+É a base de qualquer troca de dados na Web e um protocolo cliente-servidor, o que significa que as requisições são iniciadas pelo destinatário,
+ geralmente um navegador da Web.
+
+
+"""
